9b9b2546a3852b8c760469db0d946ef3872b5a56
Berk Uzuncaova <berkuzuncaova@hotmail.com>
2020-04-28 21:08:37 -0700

    updated: using sorted array for anagram

--- Anagram.java
+++ Anagram.java
@@ -1,35 +1,51 @@
+import java.io.*;
+import java.util.*;
+
 public class Anagram {
    public static void main(String[] args) {
    
-      anagram("abc", "cba");
-      anagram("ac", "cba");
-      anagram("abcabc", "cba");
+      testDriver("", "", true);
+      testDriver("abc", "", false);
+      testDriver("", "cba", false);
+      testDriver("abc", "cba", true);
+      testDriver("a", "a", true);
+      testDriver("ac", "cba", false);
+      testDriver("abcabc", "cba", false);
    }
    
-   public static void anagram(String a, String b) {
-      if (a.length() != b.length()) {
-         result("NO");
-         return;
-      }
-      while (a.length() == b.length()) {
+   public static void testDriver(String a, String b, boolean expected) {
+      System.out.println((anagram(a, b) == expected) + " - a: " + a + ", b: " + b);
+   }
+   
+   public static boolean anagram(String a, String b) {
+      int s1 = a.length();
+      int s2 = b.length();
       
-         char letter = a.charAt(0);
-         String s = letter + "";
-         
-         if (b.indexOf(letter) > -1) {
-            b.replaceFirst(s, "");
-            a = a.substring(1);
-         } 
-         else {
-            result("NO");
-            return;
+      if (s1 != s2) {
+         return false;
+      }
+      
+      if (s1 == 0) {
+         return true;
+      }
+      
+      char[] arrayA = new char[s1];
+      char[] arrayB = new char[s2];
+      
+      for (int i = 0; i < s1; i++) {
+         arrayA[i] = a.charAt(i);
+         arrayB[i] = b.charAt(i);
+      }
+      
+      Arrays.sort(arrayA);
+      Arrays.sort(arrayB);
+      
+      for (int i = 0; i < s1; i++) {
+         if (arrayA[i] != arrayB[i]) {
+            return false;
          }
-      }
+      }  
       
-      result("YES");
-   }
-   
-   public static void result(String s) {
-      System.out.println(s);
+      return true;
    }
 }
